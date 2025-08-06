7e1241aa77fab3f8c7da26bff1675f74a9ac7e8f
rjhae05 <74582706+rjhae05@users.noreply.github.com>
2025-08-06 22:09:12 +0800

    Update firebaseAdmin.js

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,7 +1,8 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+const serviceAccount = JSON.parse(process.env.SMART_MINUTES_DATABASE_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
+

fea1ce00fdd630567be9cfd69b637a78cedd39f1
rjhae05 <74582706+rjhae05@users.noreply.github.com>
2025-08-06 22:57:10 +0800

    Update firebaseAdmin.js

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const admin = require("firebase-admin");
 const fs = require("fs");
-const path = "/etc/secrets/smart-minutes-database-key"; // or .js or whatever extension is used
+const path = "/etc/secrets/smart-minutes-database-key.json"; // or .js or whatever extension is used
 
 let serviceAccount;
 
@@ -24,4 +24,5 @@ admin.initializeApp({
 });
 
 module.exports = admin;
+
 

230e457e74b43f9395ee96b9c0fcb0fb1edf7f06
rjhae05 <74582706+rjhae05@users.noreply.github.com>
2025-08-07 13:47:28 +0800

    Update firebaseAdmin.js

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,10 +1,11 @@
-var admin = require("firebase-admin");
 
-var serviceAccount = require("/etc/secrets/smart-minutes-database-key.json");
+// firebaseAdmin.js
+const admin = require("firebase-admin");
+const serviceAccount = require("/etc/secrets/smart-minutes-database-key.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://smartminutesdatabase-default-rtdb.firebaseio.com"
 });
 
-const db = admin.database(); 
+module.exports = admin;
